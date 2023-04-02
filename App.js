a74e579806f0e7f9111f41c66d730ff6bec7e708
ALEEF02 <thesmarty2002@gmail.com>
2023-04-02 10:03:33 -0400

    Add code to rotate through the screens

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  MasterNutritionToday,
+  MasterNutritionWeek,
+  MasterUserProfile,
+  MasterMind,
+  MasterBodySleep,
+  MasterBody,
+  MasterNutritionCameraInput,
+  MasterHome,
+  MasterShowKPIsAdjustHabits,
+  MasterHealthDataPermissions,
+  MasterFocuses,
+  MasterQuestionHoursofSleep,
+  MasterQuestionActivityLevel,
+  MasterQuestionSex,
+  MasterQuestionWeight,
+  MasterQuestionHeight,
+  MasterQuestionBirthday,
+  MasterQuestionName,
+  MasterLandingPage,
+} from './src/figmaComponents';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const components = [
+  MasterLandingPage,
+  MasterQuestionName,
+  MasterQuestionBirthday,
+  MasterQuestionHeight,
+  MasterQuestionWeight,
+  MasterQuestionSex,
+  MasterQuestionActivityLevel,
+  MasterQuestionHoursofSleep,
+  MasterFocuses,
+  MasterNutritionToday,
+  MasterNutritionWeek,
+  MasterHealthDataPermissions,
+  MasterShowKPIsAdjustHabits,
+  MasterNutritionCameraInput,
+  MasterBodySleep,
+  MasterBody,
+  MasterMind,
+  MasterHome,
+  MasterUserProfile,
+];
+
+function App() {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index) => (index + 1) % components.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const Component = components[index];
+
+  return <Component />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
